af3ea0d330cb783489e4b5d9c7a2cbc785739b32
kislyakovm <kislyakovm97@gmail.com>
2023-12-14 02:41:23 +0600

    update

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,7 +6,12 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+    if (studentID <= 10) {
+        return Student.getValueOf(studentID);
+    } else {
+        throw new StudentNotFoundException("Could not find student with ID " + studentID);
+    }
+
   }
 
   public static void main(String[] args) {
